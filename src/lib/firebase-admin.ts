3b165a1bcf0016de19c019929d21b7bc1e196249
mayilss <mayil.s@bestcomp.net>
2025-04-15 10:11:46 +0400

    replace \n from private key

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,7 +3,7 @@ import * as admin from "firebase-admin";
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 if (!admin.apps.length) {
